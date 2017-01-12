f3b1ed7aa183f65b5c26061b00d9ba0c51683cd6
RhythmOfRiora <cburn01@qub.ac.uk>
2017-01-12 11:02:16 +0000

    Inserted a GPL license and disclaimers into .py files.
    I just had my eyes opened to the world of licensing, so here we go!

--- Temperature_Sensor/Temperature_Sensor.py
+++ Temperature_Sensor/Temperature_Sensor.py
@@ -1,3 +1,23 @@
+# This is a 1-Wire Temperature Sensor which produces real-time graphs. It uses a variety of Maxim hardware detailed
+# in the docs.
+#     Copyright (C) <2017>  <C. Burn>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#     Please contact cburn01@qub.ac.uk for any comments or queries.
+
+
 from __future__ import division
 import serial
 import serial.tools.list_ports
